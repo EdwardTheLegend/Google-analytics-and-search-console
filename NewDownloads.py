43e71339d8a59c79da26837638294f8fb1c72ad1
raymondlowe <49975086+raymondlowe@users.noreply.github.com>
2019-06-20 20:03:19 +0800

    Support multiple google account 2 (#14)
    * One user name by arg
    
    This allows a single user name at a time, by having separate matching client_secret.json, analytics.dat and webmasters.dat per that username. Output file names are also preappended with the username string.
    
    * readme credentials
    
    updated instructions with click by click how to make credentials as that is one of the most confusing things
    
    * Multiple users from file
    
    Allow the -g arg to take a filename, and then use each line of the file to be used as a google account value.
    
    * Update README.md
    
    * Multi user from file support
    
    It can use the -g arg to specify a text file, then will itterate through all those lines and use each to find the right client secret.
    
    * Remove empty lines in input txt file
    
    * Update .gitignore

--- NewDownloads.py
+++ NewDownloads.py
@@ -26,8 +26,9 @@ parser.add_argument("-t", "--type", default="web", choices=("image","video","web
 #parser.add_argument("-f","--filters",default=2,type=int, help="Minimum number for metric, default is 2")
 parser.add_argument("-d","--dimensions",default="page", help="The dimensions are the left hand side of the table, default is page")
 #parser.add_argument("-m","--metrics",default="pageviews", help="The metrics are the things on the left, default is pageviews")
-parser.add_argument("-n","--name",default='search-console' + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),type=str, help="File name for final output, default is finaloutput + the current date. You do NOT need to add file extension.")
+parser.add_argument("-n","--name",default='search-console-' + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),type=str, help="File name for final output, default is search-console- + the current date. You do NOT need to add file extension.")
 #parser.add_argument("-c", "--clean", action="count", default=0, help="clean output skips header and count and just sends csv rows")
+parser.add_argument("-g","--googleaccount",type=str, default="", help="Name of a google account; does not have to literally be the account name but becomes a token to access that particular set of secrets. Client secrets will have to be in this a file that is this string concatenated with client_secret.json.  OR if this is the name of a text file then every line in the text file is processed as one user and all results appended together into a file file")
 
 args = parser.parse_args()
 
@@ -38,6 +39,7 @@ dimensions = args.dimensions
 #metrics = args.metrics
 name = args.name
 dataType = args.type
+googleaccountstring = args.googleaccount
 
 ## test vars defined here
 # start_date = '2019-04-01'
@@ -49,55 +51,83 @@ dataType = args.type
 ## end test vars
 
 scope = ['https://www.googleapis.com/auth/webmasters.readonly']
-# Authenticate and construct service.
-service = get_service('webmasters', 'v3', scope, 'client_secrets.json')
-profiles = service.sites().list().execute()
-#profiles is now list    
-
-print(len(profiles['siteEntry']))
-
-bar = IncrementalBar('Processing',max=len(profiles['siteEntry']))
 
 
-bigdf = pd.DataFrame()
+try:
+    googleaccountslist = open(googleaccountstring).read().splitlines()
+    # remove empty lines
+    googleaccountslist = [x.strip() for x in googleaccountslist if x.strip()]
+except:
+    googleaccountslist = [googleaccountstring]
 
-for item in profiles['siteEntry']:
-    bar.next()
-    if item['permissionLevel'] != 'siteUnverifiedUser':
+print(googleaccountslist)
 
-        smalldf = pd.DataFrame()
+combinedDF = pd.DataFrame()
 
-        #print(item['id'] + ',' + start_date + ',' + end_date)
-        results = service.searchanalytics().query(
-        siteUrl=item['siteUrl'], body={
-            'startDate': start_date,
-            'endDate': end_date,
-            'dimensions': [dimensions],
-            'searchType': dataType,
-            'rowLimit': 5000
-        }).execute()
+for thisgoogleaccount in googleaccountslist:
+    print(thisgoogleaccount)
+    # Authenticate and construct service.
+    service = get_service('webmasters', 'v3', scope, 'client_secrets.json', thisgoogleaccount)
+    profiles = service.sites().list().execute()
+    #profiles is now list    
 
-        if len(results) == 2:
-            #print(results['rows'])
-            #print(smalldf)
-            smalldf = smalldf.append(results['rows'])
-            #print(smalldf)
-        
-            smalldf.insert(0,'siteUrl',item['siteUrl'])
-            #print(smalldf)
-            if len(bigdf.columns) == 0:
-                bigdf = smalldf.copy()
-            else:
-                bigdf = pd.concat([bigdf,smalldf])
+    print("Len Profiles siteEntry: " + str(len(profiles['siteEntry'])))
 
-            #print(bigdf)
-bar.finish()
+    bar = IncrementalBar('Processing',max=len(profiles['siteEntry']))
 
-bigdf.reset_index()
-#bigdf.to_json("output.json",orient="records")
 
-bigdf['keys'] = bigdf["keys"].str[0]
+    bigdf = pd.DataFrame()
 
-bigdf.to_excel(name + '.xlsx', sheet_name='data')
+    for item in profiles['siteEntry']:
+        bar.next()
+        if item['permissionLevel'] != 'siteUnverifiedUser':
+
+            smalldf = pd.DataFrame()
+
+            #print(item['id'] + ',' + start_date + ',' + end_date)
+            results = service.searchanalytics().query(
+            siteUrl=item['siteUrl'], body={
+                'startDate': start_date,
+                'endDate': end_date,
+                'dimensions': [dimensions],
+                'searchType': dataType,
+                'rowLimit': 5000
+            }).execute()
+
+            if len(results) == 2:
+                #print(results['rows'])
+                #print(smalldf)
+                smalldf = smalldf.append(results['rows'])
+                #print(smalldf)
+            
+                smalldf.insert(0,'siteUrl',item['siteUrl'])
+                #print(smalldf)
+                if len(bigdf.columns) == 0:
+                    bigdf = smalldf.copy()
+                else:
+                    bigdf = pd.concat([bigdf,smalldf])
+
+                #print(bigdf)
+    bar.finish()
+
+    bigdf.reset_index()
+    #bigdf.to_json("output.json",orient="records")
+
+    bigdf['keys'] = bigdf["keys"].str[0]
+
+    # Got the bigdf now of all the data from this account, so add it into the combined
+    combinedDF = pd.concat([combinedDF,bigdf],sort=True)
+
+    # clean up objects used in this pass
+    del bigdf
+    del profiles
+    del service
+
+
+
+if googleaccountstring > "" :
+    name = googleaccountstring + "-" + name 
+
+combinedDF.to_excel(name + '.xlsx', sheet_name='data')
 print("finished")
 
--- googleAPIget_service.py
+++ googleAPIget_service.py
@@ -7,7 +7,7 @@ from oauth2client import tools
 from oauth2client import client
 
 
-def get_service(api_name, api_version, scope, client_secrets_path):
+def get_service(api_name, api_version, scope, client_secrets_path, usernameToken = ""):
   """Get a service that communicates to a Google API.
 
   Args:
@@ -28,14 +28,14 @@ def get_service(api_name, api_version, scope, client_secrets_path):
 
   # Set up a Flow object to be used if we need to authenticate.
   flow = client.flow_from_clientsecrets(
-      client_secrets_path, scope=scope,
-      message=tools.message_if_missing(client_secrets_path))
+      usernameToken+"-"+client_secrets_path, scope=scope,
+      message=tools.message_if_missing(usernameToken+"-"+client_secrets_path))
 
   # Prepare credentials, and authorize HTTP object with them.
   # If the credentials don't exist or are invalid run through the native client
   # flow. The Storage object will ensure that if successful the good
   # credentials will get written back to a file.
-  storage = file.Storage(api_name + '.dat')
+  storage = file.Storage(usernameToken+"-"+api_name + '.dat')
   credentials = storage.get()
   if credentials is None or credentials.invalid:
     credentials = tools.run_flow(flow, storage, flags)
