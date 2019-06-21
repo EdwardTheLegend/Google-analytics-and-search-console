50a161bfb41ae06e128d85a5f533222c4196dc2a
raymondlowe <49975086+raymondlowe@users.noreply.github.com>
2019-06-21 13:08:49 +0800

    bugfix

--- googleAPIget_service.py
+++ googleAPIget_service.py
@@ -26,16 +26,27 @@ def get_service(api_name, api_version, scope, client_secrets_path, usernameToken
       parents=[tools.argparser])
   flags = parser.parse_args([])
 
+  if usernameToken == "":
+    combined_client_secrets_path = client_secrets_path
+  else:
+    combined_client_secrets_path = usernameToken+"-"+client_secrets_path
+
   # Set up a Flow object to be used if we need to authenticate.
   flow = client.flow_from_clientsecrets(
-      usernameToken+"-"+client_secrets_path, scope=scope,
-      message=tools.message_if_missing(usernameToken+"-"+client_secrets_path))
+      combined_client_secrets_path, scope=scope,
+      message=tools.message_if_missing(combined_client_secrets_path))
 
   # Prepare credentials, and authorize HTTP object with them.
   # If the credentials don't exist or are invalid run through the native client
   # flow. The Storage object will ensure that if successful the good
   # credentials will get written back to a file.
-  storage = file.Storage(usernameToken+"-"+api_name + '.dat')
+
+  if usernameToken == "":
+    combined_data_file_name = api_name + '.dat'
+  else:
+    combined_data_file_name = usernameToken+"-"+api_name + '.dat'
+
+  storage = file.Storage(combined_data_file_name)
   credentials = storage.get()
   if credentials is None or credentials.invalid:
     credentials = tools.run_flow(flow, storage, flags)
