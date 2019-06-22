05aed3a488533b6fd31eae79b08591e5f9bcfad2
Edward Lowe <edward@edwardtlowe.com>
2019-06-22 11:33:55 +0800

    Update NewDownloads.py

--- NewDownloads.py
+++ NewDownloads.py
@@ -27,7 +27,7 @@ parser.add_argument("-t", "--type", default="web", choices=("image","video","web
 #parser.add_argument("-f","--filters",default=2,type=int, help="Minimum number for metric, default is 2")
 parser.add_argument("-d","--dimensions",default="page", help="The dimensions are the left hand side of the table, default is page. Options are date, query, page, country, device")
 #parser.add_argument("-m","--metrics",default="pageviews", help="The metrics are the things on the left, default is pageviews")
-parser.add_argument("-n","--name",default='search-console-[dimensions]-[datestring]',type=str, help="File name for final output, default is search-console- + the current date. You do NOT need to add file extension.")
+parser.add_argument("-n","--name",default='search-console-[dimensions]-[datestring]',type=str, help="File name for final output, default is search-console- + the current date. You do NOT need to add file extension")
 #parser.add_argument("-c", "--clean", action="count", default=0, help="clean output skips header and count and just sends csv rows")
 parser.add_argument("-g","--googleaccount",type=str, default="", help="Name of a google account; does not have to literally be the account name but becomes a token to access that particular set of secrets. Client secrets will have to be in this a file that is this string concatenated with client_secret.json.  OR if this is the name of a text file then every line in the text file is processed as one user and all results appended together into a file file")
 
