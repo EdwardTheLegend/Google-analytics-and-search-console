3c75d9f7d807903c6261b47b075d6589eb4e710b
Edward Lowe <edward@edwardtlowe.com>
2019-06-22 11:12:33 +0800

    Update NewDownloads.py

--- NewDownloads.py
+++ NewDownloads.py
@@ -16,19 +16,24 @@ win_unicode_console.enable()
 
 parser = argparse.ArgumentParser()
 
-#when doing argument parsing in command terminal put python before file name. No idea why, so just do it.
+# when doing argument parsing in command terminal put python before file name. No idea why, so just do it.
 
 
-#parser.add_argument("viewProfileID",type=int, help="GA View (profile) ID as a number") !!!already got this from loop!!!
-parser.add_argument("start_date", help="start date in format yyyy-mm-dd or 'yesterday' '7DaysAgo'")
-parser.add_argument("end_date", help="start date in format yyyy-mm-dd or 'today'")
-parser.add_argument("-t", "--type", default="web", choices=("image","video","web"), help="Search types for the returned data, default is web")
+# parser.add_argument("viewProfileID",type=int, help="GA View (profile) ID as a number") !!!already got this from loop!!!
+parser.add_argument(
+    "start_date", help="start date in format yyyy-mm-dd or 'yesterday' '7DaysAgo'")
+parser.add_argument(
+    "end_date", help="start date in format yyyy-mm-dd or 'today'")
+parser.add_argument("-t", "--type", default="web", choices=("image", "video",
+                                                            "web"), help="Search types for the returned data, default is web")
 #parser.add_argument("-f","--filters",default=2,type=int, help="Minimum number for metric, default is 2")
-parser.add_argument("-d","--dimensions",default="page", help="The dimensions are the left hand side of the table, default is page")
+parser.add_argument("-d", "--dimensions", default="page",
+                    help="The dimensions are the left hand side of the table, default is page")
 #parser.add_argument("-m","--metrics",default="pageviews", help="The metrics are the things on the left, default is pageviews")
-parser.add_argument("-n","--name",default='search-console-' + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),type=str, help="File name for final output, default is search-console- + the current date. You do NOT need to add file extension.")
+parser.add_argument("-n", "--name", default='search-console-' + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"), type=str,
+                    help="File name for final output, default is search-console- + the current date. You do NOT need to add file extension.")
 #parser.add_argument("-c", "--clean", action="count", default=0, help="clean output skips header and count and just sends csv rows")
-parser.add_argument("-g","--googleaccount",type=str, default="", help="Name of a google account; does not have to literally be the account name but becomes a token to access that particular set of secrets. Client secrets will have to be in this a file that is this string concatenated with client_secret.json.  OR if this is the name of a text file then every line in the text file is processed as one user and all results appended together into a file file")
+parser.add_argument("-g", "--googleaccount", type=str, default="", help="Name of a google account; does not have to literally be the account name but becomes a token to access that particular set of secrets. Client secrets will have to be in this a file that is this string concatenated with client_secret.json.  OR if this is the name of a text file then every line in the text file is processed as one user and all results appended together into a file file")
 
 args = parser.parse_args()
 
@@ -41,14 +46,14 @@ name = args.name
 dataType = args.type
 googleaccountstring = args.googleaccount
 
-## test vars defined here
+# test vars defined here
 # start_date = '2019-04-01'
 # end_date = '2019-04-07'
 
 # dimensions = 'page'
 # name = 'output'
 
-## end test vars
+# end test vars
 
 scope = ['https://www.googleapis.com/auth/webmasters.readonly']
 
@@ -67,14 +72,14 @@ combinedDF = pd.DataFrame()
 for thisgoogleaccount in googleaccountslist:
     print(thisgoogleaccount)
     # Authenticate and construct service.
-    service = get_service('webmasters', 'v3', scope, 'client_secrets.json', thisgoogleaccount)
+    service = get_service('webmasters', 'v3', scope,
+                          'client_secrets.json', thisgoogleaccount)
     profiles = service.sites().list().execute()
-    #profiles is now list    
+    # profiles is now list
 
     print("Len Profiles siteEntry: " + str(len(profiles['siteEntry'])))
 
-    bar = IncrementalBar('Processing',max=len(profiles['siteEntry']))
-
+    bar = IncrementalBar('Processing', max=len(profiles['siteEntry']))
 
     bigdf = pd.DataFrame()
 
@@ -86,37 +91,37 @@ for thisgoogleaccount in googleaccountslist:
 
             #print(item['id'] + ',' + start_date + ',' + end_date)
             results = service.searchanalytics().query(
-            siteUrl=item['siteUrl'], body={
-                'startDate': start_date,
-                'endDate': end_date,
-                'dimensions': [dimensions],
-                'searchType': dataType,
-                'rowLimit': 5000
-            }).execute()
+                siteUrl=item['siteUrl'], body={
+                    'startDate': start_date,
+                    'endDate': end_date,
+                    'dimensions': [dimensions],
+                    'searchType': dataType,
+                    'rowLimit': 5000
+                }).execute()
 
             if len(results) == 2:
-                #print(results['rows'])
-                #print(smalldf)
+                # print(results['rows'])
+                # print(smalldf)
                 smalldf = smalldf.append(results['rows'])
-                #print(smalldf)
-            
-                smalldf.insert(0,'siteUrl',item['siteUrl'])
-                #print(smalldf)
+                # print(smalldf)
+
+                smalldf.insert(0, 'siteUrl', item['siteUrl'])
+                # print(smalldf)
                 if len(bigdf.columns) == 0:
                     bigdf = smalldf.copy()
                 else:
-                    bigdf = pd.concat([bigdf,smalldf])
+                    bigdf = pd.concat([bigdf, smalldf])
 
-                #print(bigdf)
+                # print(bigdf)
     bar.finish()
 
     bigdf.reset_index()
-    #bigdf.to_json("output.json",orient="records")
+    # bigdf.to_json("output.json",orient="records")
 
     bigdf['keys'] = bigdf["keys"].str[0]
 
     # Got the bigdf now of all the data from this account, so add it into the combined
-    combinedDF = pd.concat([combinedDF,bigdf],sort=True)
+    combinedDF = pd.concat([combinedDF, bigdf], sort=True)
 
     # clean up objects used in this pass
     del bigdf
@@ -124,10 +129,8 @@ for thisgoogleaccount in googleaccountslist:
     del service
 
 
-
-if googleaccountstring > "" :
-    name = googleaccountstring + "-" + name 
+if googleaccountstring > "":
+    name = googleaccountstring + "-" + name
 
 combinedDF.to_excel(name + '.xlsx', sheet_name='data')
 print("finished")
-
